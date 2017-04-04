7e196fa9bea263bd270ee08c94ec85da983f12a5
omrobbie <putu.roby@yahoo.com>
2017-04-04 20:02:52 +0800

    running test script

--- tests/TennisGameTest.php
+++ tests/TennisGameTest.php
@@ -26,5 +26,75 @@ class TennisGameTest extends PHPUnit_Framework_TestCase
         $this->game->wonPoint('Eric');
         $this->assertEquals($this->game->getScore(), 'Fifteen-Love');
     }
-}
 
+    public function testSecondPlayerWinFirstGame(){
+        $this->game->wonPoint('John');
+        $this->assertEquals($this->game->getScore(), 'Love-Fifteen');
+    }
+
+    public function testFirstPlayerWinFirstGameInARow(){
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('Eric');
+        $this->assertEquals($this->game->getScore(), 'Thirty-Love');
+    }
+
+    public function testSecondPlayerWinFirstGameInARow(){
+        $this->game->wonPoint('John');
+        $this->game->wonPoint('John');
+        $this->assertEquals($this->game->getScore(), 'Love-Thirty');
+    }
+
+    public function testDrawInAFirstGame(){
+        $this->assertEquals($this->game->getScore(), 'Love-All');
+    }
+
+    public function testFifteenDrawInAFirstGame(){
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('John');
+        $this->assertEquals($this->game->getScore(), 'Fifteen-All');
+    }
+
+    public function testThirtyDrawInAFirstGame(){
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('John');
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('John');
+        $this->assertEquals($this->game->getScore(), 'Thirty-All');
+    }
+
+    public function testDeuceDrawInAFirstGame(){
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('John');
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('John');
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('John');
+        $this->assertEquals($this->game->getScore(), 'Deuce');
+    }
+
+    public function testFirstPlayerAdvantage(){
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('Jhon');
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('Jhon');
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('Jhon');
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('Jhon');
+        $this->game->wonPoint('Eric');
+        $this->assertEquals($this->game->getScore(), 'Advantage player1');
+    }
+
+    public function testSecondPlayerAdvantage(){
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('Jhon');
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('Jhon');
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('Jhon');
+        $this->game->wonPoint('Eric');
+        $this->game->wonPoint('Jhon');
+        $this->game->wonPoint('Jhon');
+        $this->assertEquals($this->game->getScore(), 'Advantage player2');
+    }
+}
